05041f749cfd6c563fdf6f47ec03fbd22fd1d108
NGUYEN PHU HAI <nguyenphuhai89@gmail.com>
2024-05-08 10:24:15 +0700

    video38

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,78 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss'
+import {getAllUsers} from '../../services/userService';
 class UserManage extends Component {
 
-    state = {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        console.log('get user from node.js : ', response);
+        if(response && response.errCode===0){
+            this.setState({
+                arrUsers: response.users
+            }, () => {
+                console.log(this.state.arrUsers)
+            })
+            
+            console.log(this.state.arrUsers)
+        }
     }
-
+    /** Life cycle
+    * Run component
+    * 1. Run constructor ->init state
+    * 2. Did mount (set state)
+    * 3. Render 
+    * 
+    */
 
     render() {
+        console.log('check render', this.state)
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className='users-container'>
+                <div className="title text-center">Manage users with react </div>
+                <div className="users-table mt-3 mx-1">
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        
+                            {
+                                arrUsers && arrUsers.map((item,index) =>{
+                                    console.log('eric check map',item, index)
+                                    return(
+                                        <tr>
+                                            <td>{item.email}</td>
+                                            <td>{item.firstName}</td>
+                                            <td>{item.lastName}</td>
+                                            <td>{item.address}</td>
+                                            <td>
+                                                <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
+                                                <button className='btn-delete'><i class="fas fa-trash-alt"></i></button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }                            
+                        
+                        
+                        </table>
+                </div>
+            </div>
+            
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -6,4 +6,10 @@ import axios from '../axios';
 const handleLoginApi = (email, password) => {
     return axios.post('/api/login',{email, password})
 }
-export {handleLoginApi} 
+
+const getAllUsers = (inputId) => {
+    // template string
+    return axios.get(`/api/get-all-users?id=${inputId}`)
+}
+
+export {handleLoginApi, getAllUsers} 
