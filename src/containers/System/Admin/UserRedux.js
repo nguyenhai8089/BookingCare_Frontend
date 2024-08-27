da44d36d9d4f8a7a2aaa48c34fa9385eab7e76f8
NGUYEN PHU HAI <nguyenphuhai89@gmail.com>
2024-08-27 17:30:25 +0700

    video58

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -1,19 +1,29 @@
+/** @format */
+
 import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
-import { getAllCodeService } from "../../../services/userService";
+import "./UserRedux.scss";
 import { LANGUAGES } from "../../../utils/";
 import * as actions from "../../../store/actions";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css"; // This only needs to be imported once in your app
 class UserRedux extends Component {
       constructor(props) {
             super(props);
             this.state = {
                   genderArr: [],
+                  positionArr: [],
+                  roleArr: [],
+                  previewImgURL: "",
+                  isOpen: false,
             };
       }
 
       async componentDidMount() {
             this.props.getGenderStart();
+            this.props.getPositionStart();
+            this.props.getRoleStart();
             // try {
             //       let res = await getAllCodeService("gender");
             //       if (res && res.errCode === 0) {
@@ -35,147 +45,161 @@ class UserRedux extends Component {
                         genderArr: this.props.genderRedux,
                   });
             }
+            if (prevProps.positionRedux !== this.props.positionRedux) {
+                  this.setState({
+                        positionArr: this.props.positionRedux,
+                  });
+            }
+            if (prevProps.roleRedux !== this.props.roleRedux) {
+                  this.setState({
+                        roleArr: this.props.roleRedux,
+                  });
+            }
       }
 
+      Loading = () => {
+            return (
+                  <div className='col-12'>
+                        <span className='fa fa-spinner fa-pulse fa-3x fa-fw text-primary'></span>
+                        <p>Loading . . .</p>
+                  </div>
+            );
+      };
+      handleOnchangeImage = (event) => {
+            let data = event.target.files;
+            let file = data[0];
+            if (file) {
+                  let objectUrl = URL.createObjectURL(file);
+                  this.setState({
+                        previewImgURL: objectUrl,
+                  });
+            }
+
+            // console.log("checking file 0 : ", objectUrl);
+      };
+
+      openPreviewImage = () => {
+            if (!this.state.previewImgURL) return null;
+            this.setState({
+                  isOpen: true,
+            });
+      };
       render() {
             let genders = this.state.genderArr;
+            let positions = this.state.positionArr;
+            let roles = this.state.roleArr;
             // let genders = this.props.genderRedux;
             let language = this.props.language;
-            console.log(
-                  "hoidanit check props from redux : ",
-                  this.props.genderRedux
-            );
+            let isLoadingGenderReact = this.props.isLoadingGender;
+            console.log("hoidanit check props from redux : ", this.props);
             return (
-                  <div className="user-redux-container">
-                        <div className="title">
-                              User Redux Hỏi Dân IT YouTuBe Channel
-                        </div>
-                        <div className="user-redux-body">
-                              <div className="container">
-                                    <div className="row">
-                                          <div className="col-12 my-3">
-                                                <FormattedMessage id="manage-user.add" />
+                  <div className='user-redux-container'>
+                        <div className='title'>User Redux Hỏi Dân IT YouTuBe Channel</div>
+                        <div className='title'>{isLoadingGenderReact === true ? <this.Loading /> : ""}</div>
+                        <div className='user-redux-body'>
+                              <div className='container'>
+                                    <div className='row'>
+                                          <div className='col-12 my-3'>
+                                                <FormattedMessage id='manage-user.add' />
                                           </div>
-                                          <div className="col-3">
+                                          <div className='col-3'>
                                                 <label>
-                                                      <FormattedMessage id="manage-user.email" />
+                                                      <FormattedMessage id='manage-user.email' />
                                                 </label>
-                                                <input
-                                                      className="form-control"
-                                                      type="email"
-                                                />
+                                                <input className='form-control' type='email' />
                                           </div>
-                                          <div className="col-3">
+                                          <div className='col-3'>
                                                 <label>
-                                                      <FormattedMessage id="manage-user.password" />
+                                                      <FormattedMessage id='manage-user.password' />
                                                 </label>
-                                                <input
-                                                      className="form-control"
-                                                      type="password"
-                                                />
+                                                <input className='form-control' type='password' />
                                           </div>
-                                          <div className="col-3">
+                                          <div className='col-3'>
                                                 <label>
-                                                      <FormattedMessage id="manage-user.first-name" />
+                                                      <FormattedMessage id='manage-user.first-name' />
                                                 </label>
-                                                <input
-                                                      className="form-control"
-                                                      type="text"
-                                                />
+                                                <input className='form-control' type='text' />
                                           </div>
-                                          <div className="col-3">
+                                          <div className='col-3'>
                                                 <label>
-                                                      <FormattedMessage id="manage-user.last-name" />
+                                                      <FormattedMessage id='manage-user.last-name' />
                                                 </label>
-                                                <input
-                                                      className="form-control"
-                                                      type="text"
-                                                />
+                                                <input className='form-control' type='text' />
                                           </div>
-                                          <div className="col-3">
+                                          <div className='col-3'>
                                                 <label>
-                                                      <FormattedMessage id="manage-user.phone-number" />
+                                                      <FormattedMessage id='manage-user.phone-number' />
                                                 </label>
-                                                <input
-                                                      className="form-control"
-                                                      type="text"
-                                                />
+                                                <input className='form-control' type='text' />
                                           </div>
-                                          <div className="col-9">
+                                          <div className='col-9'>
                                                 <label>
-                                                      <FormattedMessage id="manage-user.address" />
+                                                      <FormattedMessage id='manage-user.address' />
                                                 </label>
-                                                <input
-                                                      className="form-control"
-                                                      type="text"
-                                                />
+                                                <input className='form-control' type='text' />
                                           </div>
-                                          <div className="col-3">
+                                          <div className='col-3'>
                                                 <label>
-                                                      <FormattedMessage id="manage-user.gender" />
+                                                      <FormattedMessage id='manage-user.gender' />
                                                 </label>
-                                                <select className="form-control">
+                                                <select className='form-control'>
                                                       {genders &&
-                                                            genders.length >
-                                                                  0 &&
-                                                            genders.map(
-                                                                  (
-                                                                        item,
-                                                                        index
-                                                                  ) => {
-                                                                        return (
-                                                                              <option
-                                                                                    key={
-                                                                                          index
-                                                                                    }
-                                                                              >
-                                                                                    {language ===
-                                                                                    LANGUAGES.VI
-                                                                                          ? item.valueVi
-                                                                                          : item.valueEn}
-                                                                              </option>
-                                                                        );
-                                                                  }
-                                                            )}
+                                                            genders.length > 0 &&
+                                                            genders.map((item, index) => {
+                                                                  return <option key={index}>{language === LANGUAGES.VI ? item.valueVi : item.valueEn}</option>;
+                                                            })}
                                                 </select>
                                           </div>
-                                          <div className="col-3">
+                                          <div className='col-3'>
                                                 <label>
-                                                      <FormattedMessage id="manage-user.position" />
+                                                      <FormattedMessage id='manage-user.position' />
                                                 </label>
-                                                <select className="form-control">
-                                                      <option>Choose...</option>
-                                                      <option>M</option>
-                                                      <option>F</option>
+                                                <select className='form-control'>
+                                                      {positions &&
+                                                            positions.length > 0 &&
+                                                            positions.map((item, index) => {
+                                                                  return <option key={index}>{language === LANGUAGES.VI ? item.valueVi : item.valueEn}</option>;
+                                                            })}
                                                 </select>
                                           </div>
-                                          <div className="col-3">
+                                          <div className='col-3'>
                                                 <label>
-                                                      <FormattedMessage id="manage-user.role" />
+                                                      <FormattedMessage id='manage-user.role' />
                                                 </label>
-                                                <select className="form-control">
-                                                      <option>Choose...</option>
-                                                      <option>M</option>
-                                                      <option>F</option>
+                                                <select className='form-control'>
+                                                      {roles &&
+                                                            roles.length > 0 &&
+                                                            roles.map((item, index) => {
+                                                                  return <option key={index}>{language === LANGUAGES.VI ? item.valueVi : item.valueEn}</option>;
+                                                            })}
                                                 </select>
                                           </div>
-                                          <div className="col-3">
+                                          <div className='col-3'>
                                                 <label>
-                                                      <FormattedMessage id="manage-user.image" />
+                                                      <FormattedMessage id='manage-user.image' />
                                                 </label>
-                                                <input
-                                                      className="form-control"
-                                                      type="text"
-                                                />
+                                                <div className='preview-img-container'>
+                                                      <input id='previewImg' type='file' hidden onChange={(event) => this.handleOnchangeImage(event)} />
+                                                      <label className='label-upload' htmlFor='previewImg'>
+                                                            Tải ảnh <i class='fa fa-upload' aria-hidden='true'></i>
+                                                      </label>
+                                                      <div
+                                                            className='preview-image'
+                                                            style={{ backgroundImage: `url(${this.state.previewImgURL})` }}
+                                                            onClick={() => {
+                                                                  this.openPreviewImage();
+                                                            }}></div>
+                                                </div>
                                           </div>
-                                          <div className="col-12 mt-3">
-                                                <button className="btn btn-primary ">
-                                                      <FormattedMessage id="manage-user.save" />
+                                          <div className='col-12 mt-3'>
+                                                <button className='btn btn-primary '>
+                                                      <FormattedMessage id='manage-user.save' />
                                                 </button>
                                           </div>
                                     </div>
                               </div>
                         </div>
+                        {this.state.isOpen === true && <Lightbox mainSrc={this.state.previewImgURL} onCloseRequest={() => this.setState({ isOpen: false })} />}
                   </div>
             );
       }
@@ -185,15 +209,17 @@ const mapStateToProps = (state) => {
       return {
             language: state.app.language,
             genderRedux: state.admin.genders,
+            positionRedux: state.admin.positions,
+            roleRedux: state.admin.roles,
+            isLoadingGender: state.admin.isLoadingGender,
       };
 };
 
 const mapDispatchToProps = (dispatch) => {
       return {
             getGenderStart: () => dispatch(actions.fetchGenderStart()),
-            // processLogout: () => dispatch(actions.processLogout()),
-            // changeLanguageAppRedux: (language) =>
-            //       dispatch(changeLanguageApp(language)),
+            getPositionStart: () => dispatch(actions.fetchPositionStart()),
+            getRoleStart: () => dispatch(actions.fetchRoleStart()),
       };
 };
 
--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,6 +1,7 @@
 import actionTypes from "../actions/actionTypes";
 
 const initialState = {
+      isLoadingGender: false,
       genders: [],
       roles: [],
       positions: [],
@@ -9,22 +10,42 @@ const initialState = {
 const adminReducer = (state = initialState, action) => {
       switch (action.type) {
             case actionTypes.FETCH_GENDER_START:
+                  state.isLoadingGender = true;
                   console.log("hoidan it fire fetch gender start: ", action);
                   return {
                         ...state,
                   };
             case actionTypes.FETCH_GENDER_SUCCESS:
-                  let copyState = { ...state };
-                  copyState.genders = action.data;
-                  console.log(
-                        "hoidan it fire fetch gender success: ",
-                        copyState
-                  );
+                  state.genders = action.data;
+                  state.isLoadingGender = false;
+                  console.log("hoidan it fire fetch gender success: ", action);
                   return {
-                        ...copyState,
+                        ...state,
                   };
             case actionTypes.FETCH_GENDER_FAILED:
                   console.log("hoidan it fire fetch gender failed: ", action);
+                  state.genders = [];
+                  state.isLoadingGender = true;
+                  return {
+                        ...state,
+                  };
+            case actionTypes.FETCH_POSITION_SUCCESS:
+                  state.positions = action.data;
+                  return {
+                        ...state,
+                  };
+            case actionTypes.FETCH_POSITION_FAILED:
+                  state.positions = [];
+                  return {
+                        ...state,
+                  };
+            case actionTypes.FETCH_ROLE_SUCCESS:
+                  state.roles = action.data;
+                  return {
+                        ...state,
+                  };
+            case actionTypes.FETCH_ROLE_FAILED:
+                  state.roles = [];
                   return {
                         ...state,
                   };
